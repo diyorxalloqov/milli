38457c232e1561c9a2097cc4f795f21ac804d755
diyorxalloqov <diyorxalloqov@gmail.com>
2023-11-06 18:17:38 +0500

    bottom nav bar done and home and catalog page is not full

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:milli/modules/bottom_navigation_bar/presentation/bottom_navbar.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(useMaterial3: true),
+        debugShowCheckedModeBanner: false,
+        home: const BottomNavbarScreen()
+        // const SignUpPage(),
+        );
+  }
+}
---
+++ lib/assets/constants/svg_images.dart
@@ -0,0 +1,17 @@
+class SvgImages {
+  static const String logo = 'assets/icons/logo.svg';
+  static const String call = 'assets/icons/call.svg';
+  static const String percent = 'assets/icons/percent.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String favourite = 'assets/icons/heart.svg';
+  static const String home = 'assets/icons/home.svg';
+  static const String menu = 'assets/icons/menu.svg';
+  static const String shopping = 'assets/icons/shopping.svg';
+  static const String user = 'assets/icons/user.svg';
+  static const String equa = 'assets/icons/equa.svg';
+  static const String medalStar = 'assets/icons/medalStar.svg';
+  static const String shoppingCart = 'assets/icons/shoppingCart.svg';
+  static const String shield = 'assets/icons/shield.svg';
+  static const String moneys = 'assets/icons/moneys.svg';
+  static const String question = 'assets/icons/question.svg';
+}
